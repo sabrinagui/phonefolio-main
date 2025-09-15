5cdfac15bc6817397f5337af2e22327ee0f07068
Sabrina Mesquita <sabrinagui.t@outlook.com>
2025-09-15 14:37:59 -0300

    updates2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -58,15 +58,14 @@ export default function Card() {
                 className="drop-shadow-md card bg-white rounded-lg px-5 py-3 gap-x-3 flex flex-col md:flex-none md:flex-row hover:-translate-y-1 hover:scale-100 duration-300 transition ease-in-out delay-150 hover:shadow-sm border border-gray-200 hover:border-gray-300"
                 href={project["project-url"]}
               >
-                <div className="rounded-full overflow-hidden flex items-center justify-center border border-gray-200 hidden md:block">
-                  <div className="card-image w-16 h-16 rounded-full overflow-hidden">
-                    <img
-                      className="w-full h-full object-cover"
-                      src={project["project-image-url"]}
-                      alt=""
-                    />
-                  </div>
+                <div className="w-16 h-16 rounded-full overflow-hidden border border-gray-200 hidden md:flex items-center justify-center">
+                  <img
+                    className="w-full h-full aspect-square object-cover"
+                    src={project["project-image-url"]}
+                    alt="Project"
+                  />
                 </div>
+                <div className="w-16 h-16 rounded-full overflow-hidden border border-gray-200 flex md:hidden items-center justify-center"></div>
                 <div className="flex flex-col justify-center">
                   <h1 className="font-medium text-lg">
                     {project["project-name"]}
@@ -96,8 +95,6 @@ export default function Card() {
           </div>
         </div>
       </div>
-      
-      
     </>
   );
 }
